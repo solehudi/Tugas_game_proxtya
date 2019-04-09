a7459b61b09a1ba1e1f761824d2c2f3d55871b18
solehudi <42959607+solehudi@users.noreply.github.com>
2019-04-09 23:16:30 +0700

    Add files via upload

---
+++ coba1.js
@@ -0,0 +1,15 @@
+var nama = "Zero"
+var peran = "penyihir"
+if (nama===""&&peran===""){
+    console.log("nama dan peran harus di isi!")
+}else if (nama==="Mikael"&&peran===""){
+    console.log("Hallo! Mikael pilih peranmu untuk memulai game")
+}else if (nama==="Nina"&&peran==="ksatria"){
+    console.log("Hallo ksatria Nina.kamu dapat menyerang dengan senjatamu")
+}else if (nama==="Danu"&&peran==="Thabib"){
+    console.log("Hallo Tabib danu, kamu akan membantu teman yang terluka")
+}else if (nama==="Zero"&&peran==="penyihir"){
+        console.log("Hallo penyihir zero ciptakan keajaiban yang membantu kemenanganmu")
+}else{
+    console.log("kamu tidak terdaftar di game ini")
+}
